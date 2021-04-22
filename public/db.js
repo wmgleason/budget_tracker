e30a9a36860c3c8b55c4f92839f76f605ec892e9
Wanda Gleason <oglissane@gmail.com>
2021-04-22 19:03:02 -0400

    Added code to handle successful db request.

--- public/db.js
+++ public/db.js
@@ -18,3 +18,59 @@ request.onupgradeneeded = function (e) {
     db.createObjectStore("BudgetStore", { autoIncrement: true });
   }
 };
+request.onerror = function (e) {
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+  console.log("check db invoked");
+
+  // Open a transaction on your BudgetStore db
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  // access your BudgetStore object
+  const store = transaction.objectStore("BudgetStore");
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll();
+  // If the request was successful
+  getAll.onsuccess = function () {
+    // If there are items in the store, we need to bulk add them when we are back online
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // If our returned response is not empty
+          if (res.length !== 0) {
+            // Open another transaction to BudgetStore with the ability to read and write
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            // Assign the current store to a variable
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            // Clear existing entries because our bulk add was successful
+            currentStore.clear();
+            console.log("Clearing store 🧹");
+          }
+        });
+    }
+  };
+}
+
+request.onsuccess = function (e) {
+  console.log("success");
+  db = e.target.result;
+
+  // Check if app is online before reading from db
+  if (navigator.onLine) {
+    console.log("Backend online! 🗄️");
+    checkDatabase();
+  }
+};
