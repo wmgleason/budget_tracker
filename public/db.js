96a151e0b9c6ffddaf9f3f0d36c764bbf7d26741
Wanda Gleason <oglissane@gmail.com>
2021-04-22 18:56:51 -0400

    Added db.js file with new request for a budget database.

---
+++ public/db.js
@@ -0,0 +1,20 @@
+let db;
+let budgetVersion;
+
+// Creating a new db request for the "budget" database.
+const request = indexedDB.open("BudgetDB", budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log("Upgrade needed in IndexDB");
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
+  }
+};
