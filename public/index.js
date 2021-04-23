a365ca0d585e332a9d07ec2bed906d4a39b8b052
Wanda Gleason <oglissane@gmail.com>
2021-04-22 20:20:28 -0400

    Removed return from fetch function.

--- public/index.js
+++ public/index.js
@@ -1,14 +1,24 @@
 let transactions = [];
 let myChart;
 
+// fetch("/api/transaction")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // save db data on global variable
+//     transactions = data;
+
+//     populateTotal();
+//     populateTable();
+//     populateChart();
+//   });
+
 fetch("/api/transaction")
-  .then((response) => {
-    return response.json();
-  })
+  .then((response) => response.json())
   .then((data) => {
     // save db data on global variable
     transactions = data;
-
     populateTotal();
     populateTable();
     populateChart();
